37b0e96dacaae1ca3b223a9c86ae097bcb798feb
zhanglijing <941257921@qq.com>
2018-08-16 16:34:43 +0800

    a

--- src/components/components.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import footer from './footer.vue' //footer
-Vue.component('v-footer',footer); //footer
-
-import header from './header.vue' //header
-Vue.component('v-header',header); //header
-
